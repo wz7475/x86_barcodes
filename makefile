5c9c4ba2ae6794943d018f138c3ea5bc117b3a3b
Wojtek Zarzecki <wojtek.zarzecki2002@gmail.com>
2022-06-10 11:36:15 +0200

    separate declaration from definition v2

--- makefile
+++ makefile
@@ -8,10 +8,10 @@ assemble: $(ASM_SRC).asm
 	$(ASMBIN) -o $(ASM_SRC).o -f elf -g -l $(ASM_SRC).lst $(ASM_SRC).asm
 
 compile: assemble main.c
-	$(CC) -m32 -c -g -O0 main.c src/img_helpers.c -std=c99
+	$(CC) -m32 -c -g -O0 main.c src/img_helpers.c src/coding_utils.c -std=c99
 
 link: compile
-	$(CC) -m32 -g -o program main.o img_helpers.o $(ASM_SRC).o
+	$(CC) -m32 -g -o program main.o img_helpers.o coding_utils.o $(ASM_SRC).o
 
 clean:
 	rm *.o
---
+++ src/coding_utils.c
@@ -0,0 +1,48 @@
+//
+// Created by wojtek on 10.06.2022.
+//
+#include "../includes/coding_utils.h"
+#include "../includes/codes_table.h"
+#include "../includes/img_helpers.h"
+#include <string.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+void code_char(int index, uint16_t *final_widths, uint8_t width_offset, uint8_t stripe_width) {
+    uint32_t width_copy = widths[index];
+    for (int i =5; i >= 0; i--){
+        final_widths[width_offset + i] = (width_copy % 10) * stripe_width;
+        width_copy /= 10;
+    }
+}
+
+void print_string_codes(uint8_t *dest_bitmap, const char *code, uint8_t code_len, uint8_t stripe_width) {
+    uint16_t *final_widths = (uint16_t *) calloc((code_len / 2) * 6+18, 2);
+//    char code[code_len] = "0134";
+    code_char(103, final_widths, 0, stripe_width); // start code
+    uint8_t loop_counter = 1; // 6 stripes were used for start code
+    uint16_t checksum = 105; // init with start code value
+    for (int i = 0; i < code_len ; i++){
+        uint8_t code_value = (code[i] - 48) * 10 + code[i+1] - 48;
+        code_char(code_value, final_widths, loop_counter * 6, stripe_width);
+        i++;
+        checksum += code_value * loop_counter;
+        loop_counter++;
+    }
+//    printf("%d\n", checksum);
+    checksum %= 103;
+    printf("%d\t", checksum);
+    printf("%d\n", widths[checksum]);
+//    debug section
+    code_char(checksum, final_widths, loop_counter *6, stripe_width);
+    code_char(104, final_widths, loop_counter *6+6, stripe_width); // stop code
+    for (int i = 0; i <( code_len / 2) * 6+6+6+6; i++){
+        if (i % 6 == 0 && i >0){
+            printf("\n");
+        }
+        printf("%d ", final_widths[i]);
+    }
+    put_row(dest_bitmap, final_widths, (code_len / 2) * 6+18, 30);
+    free(final_widths);
+}
